44060a26ad43f0e7b4a0941fe4d8f23021b5ae92
xiangxichen26 <chen.xiangxi@northeastern.edu>
2024-07-18 14:11:11 -0700

    mofify some config

--- blog.config.js
+++ blog.config.js
@@ -30,7 +30,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Kelly', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '坚持做正确的事，不急于求成', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'kellyc26.me', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ai,game,阅读,旅行,游戏,思考,Notion,AIGC,Obisidian,开发,写作,港乐,粤语,音乐,诗句,动漫', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
